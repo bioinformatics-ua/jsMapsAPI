2885e9c65fe8943965b2591cc38685cbf8f31a6c
Felgueiras <rafaelfelgueiras@ua.pt>
2015-06-07 12:24:26 +0100

    started to add support to multiple programmatic filters

--- js/filters.js
+++ js/filters.js
@@ -5,6 +5,138 @@ var Filter = function(Name, Value, Values) {
     this.Values = Values;
 };
 
+function multiFilter(inputArgs) {
+    var keys = Object.keys(inputArgs)
+    var numberKeys = keys.length;
+
+    console.log(inputArgs)
+    console.log(numberKeys);
+
+    var validFilters = 0;
+
+    // for every key
+    $.each(keys, function(index, filterName) {
+        var filterValue = inputArgs[filterName];
+
+        // check the currentKey
+        if (filterName.toLowerCase() == 'all') {
+            console.log('removing all applied filters')
+            resetFilters();
+            return;
+        }
+
+        // check if the filterName is valid
+        if (!checkFilterNameIsValid(jsonFiltersArray, filterName)) {
+            // invalid filter name
+            console.log('Invalid filter name!(' + filterName);
+        } else {
+            // if valid, check the filterValue
+            var finalParts = getAllFilterValues(filterValue);
+            validFilters++;
+        }
+    });
+
+    // apply multiple filters
+    if (validFilters == numberKeys)
+        applyMultipleFiltersProgramattically(inputArgs);
+}
+
+function applyMultipleFiltersProgramattically(filtersToApply) {
+
+    console.log(filtersToApply);
+
+    var keys = Object.keys(filtersToApply)
+    var numFiltersToApply = keys.length;
+    var countriesHaveFilter = [];
+
+    console.log(filtersToApply)
+    console.log(numFiltersToApply);
+
+    // for every key
+    $.each(keys, function(index, filterName) {
+        var filterValue = filtersToApply[filterName];
+    });
+
+    // erase all markers and countries from the map
+    var colors = [];
+    $.each(jsonCountries, function(index, currentCountry) {
+        colors[currentCountry.Country] = 'rgb(255,255,255)';
+    });
+    map.series.regions[0].setValues(colors);
+    // remove all markers from the map
+    map.removeAllMarkers();
+
+    // for each of the countries
+    $.each(jsonCountries, function(countryIndex, currentCountry) {
+        // set to 0 the number of filters
+        countriesHaveFilter[countryIndex] = 0;
+        // check if it has the needed values
+        $.each(keys, function(index, currentFilterName) {
+            var i = 0;
+            do {
+                i++;
+                var currentNameToCheck = 'Name' + i;
+                var currentValue = 'Value' + i;
+                // check if the Country has that name
+                if (!currentCountry[currentNameToCheck])
+                    break;
+
+                if (currentCountry[currentNameToCheck].toLowerCase() == currentFilterName.toLowerCase()) {
+                    // check by value
+                    if (currentCountry[currentValue] == filtersToApply[currentFilterName]) {
+                        countriesHaveFilter[countryIndex]++;
+                    }
+                }
+            } while (true)
+        });
+    });
+
+    // colour only the countris whose countriesHaveFilter[index] == numberFilters
+    $.each(jsonCountries, function(countryIndex, currentCountry) {
+        if (countriesHaveFilter[countryIndex] == numFiltersToApply) {
+            var hue = mapRange(currentCountry.Count, minCount, maxCount, 160, 220);
+            colors[currentCountry.Country] = 'hsl(' + hue + ', 100%, 50%)';
+        }
+    });
+
+    // colour the countries
+    map.series.regions[0].setValues(colors);
+
+    /*
+
+        // add only the markers who have that filter value
+        $.each(jsonMarkers, function(index, currentMarker) {
+            // check if any of the names is equal to the selected filter
+            // try to read all the names and values
+            var i = 0;
+            do {
+                i++;
+                var currentNameToCheck = 'Name' + i;
+                var currentValue = 'Value' + i;
+                // check if the Country has that name
+                if (currentMarker[currentNameToCheck] !== undefined) {
+                    if (currentMarker[currentNameToCheck] == selectedFilter.Name) {
+                        if (currentMarker[currentValue] == filterValue) {
+                            map.addMarker(index, {
+                                latLng: [currentMarker.Latitude, currentMarker.Longitude],
+                                name: currentMarker.desc,
+
+                                // set the style for this marker
+                                style: {
+                                    fill: 'green',
+                                    r: mapRange(currentMarker.Count, minCount, maxCount, minRadius, maxRadius)
+                                }
+                            });
+                        }
+                    }
+                } else {
+                    break;
+                }
+            } while (true)
+        });
+    */
+}
+
 function filter(filterName, filterValue) {
     var returning = false;
 
@@ -45,7 +177,7 @@ function filter(filterName, filterValue) {
 function checkFilterNameIsValid(jsonFilters, filterName) {
     var valid = false;
     $.each(jsonFilters, function(index, currentFilter) {
-        if (currentFilter.Name === filterName) {
+        if (currentFilter.Name.toLowerCase() === filterName.toLowerCase()) {
             filterObject = currentFilter;
             valid = true;
             return;
@@ -257,7 +389,6 @@ function setMultipleFilters(jsonFilters) {
             $("#" + buttonId + ":first-child").text($(this).text());
             $("#" + buttonId + ":first-child").val($(this).text());
             selectedMultipleFilters[filterIndex] = jsonFilters[filterIndex].Values[selectedIndex];
-            console.log(selectedMultipleFilters);
         });
     });
 
@@ -285,8 +416,6 @@ function applyMultipleFilters(selectedMultipleFilters, jsonFilters) {
     // remove all markers from the map
     map.removeAllMarkers();
 
-
-
     // for each of the countries
     $.each(jsonCountries, function(countryIndex, currentCountry) {
         // set to 0 the number of filters
@@ -312,13 +441,9 @@ function applyMultipleFilters(selectedMultipleFilters, jsonFilters) {
         });
     });
 
-    console.log('countries ' + countriesHaveFilter);
-
     // colour only the countris whose countriesHaveFilter[index] == numberFilters
     $.each(jsonCountries, function(countryIndex, currentCountry) {
-        console.log(countriesHaveFilter[countryIndex] + ' ' + numFiltersToApply);
         if (countriesHaveFilter[countryIndex] == numFiltersToApply) {
-            console.log('+');
             var hue = mapRange(currentCountry.Count, minCount, maxCount, 160, 220);
             colors[currentCountry.Country] = 'hsl(' + hue + ', 100%, 50%)';
         }
@@ -568,5 +693,4 @@ function sliderChanged() {
             });
         }
     });
-
 };
