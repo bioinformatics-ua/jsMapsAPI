bf6acd3f8900d3e6e9205bbe7b783fb26ed0bddc
Felgueiras <rafaelfelgueiras@ua.pt>
2015-06-14 14:50:15 +0100

    started working on the markers mapping

--- js/json-mapping.js
+++ js/json-mapping.js
@@ -10,33 +10,32 @@ VectorialMap.prototype.registerTransformer = function(jsonLocation) {
 }
 
 function jsonMapMarkers(json) {
-	// in this case the 'markers' are defined the top level
-	console.log(json.length);
-	// iterate through every 'marker'
-	$.each(json, function(index, currentJSON) {
-		/* MARKERS ATTRIBUTES
-			Count: 31
-			Country: "PT"
-			Latitude: 80
-			Longitude: 21
-			Name1: "YEAR"
-			Name2: "Gender"
-			Value1: "2004"
-			Value2: "T"
-			Var: "Active patients"
-			desc: "abc"
-		*/
-		// get the name of the country
-		var name = currentJSON['country'];
-		// get the count
-		var count = 0;
-		// get the latitude
-		var latitude = currentJSON['latitude'];
-		// get the longitude
-		var longitude = currentJSON['longitude'];
-		//get the description
+	// access the JSON file that specifies the mapping
+	$.getJSON("../mappingJSON/mappingMarkersSample.json", function(jsonMapping) {
 
+		// in this case the 'markers' are defined the top level
+		console.log(json.length);
+		// iterate through every 'marker'
+		$.each(json, function(index, currentJSON) {
+			// get the name of the country
+			var name = currentJSON[jsonMapping.Country];
+			// the country name must be on the two-digit format
+			// get the count - WHAT IS THE COUNT?
+			var count = 0;
+			// get the latitude
+			var latitude = currentJSON[jsonMapping.Latitude];
+			// get the longitude
+			var longitude = currentJSON[jsonMapping.Longitude];
+			//get the description - WHT IS THE DESCRIPTION OF A MARKER?
+			var description = '';
+			/*
+			WHAT ARE THE NAMES???
+			*/
+			console.log(name+' '+latitude+' '+longitude);
+
+		});
 	});
+
 	var finalJSON;
 	return finalJSON;
 }
