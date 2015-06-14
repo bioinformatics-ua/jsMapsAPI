57759d86d3564951b5bfdcc16d6befed809c14e5
Felgueiras <rafaelfelgueiras@ua.pt>
2015-06-14 14:28:00 +0100

    added sample espid file

--- js/json-mapping.js
+++ js/json-mapping.js
@@ -4,26 +4,39 @@ VectorialMap.prototype.registerTransformer = function(jsonLocation) {
 		console.log(json);
 
 		// map the markers
+		// the programmer must indicate where the countries are
 		var markers = jsonMapMarkers(json);
 	});
 }
 
 function jsonMapMarkers(json) {
-	/* MARKERS ATTRIBUTES
-		Count: 31
-		Country: "PT"
-		Latitude: 80
-		Longitude: 21
-		Name1: "YEAR"
-		Name2: "Gender"
-		Value1: "2004"
-		Value2: "T"
-		Var: "Active patients"
-		desc: "abc"
-	*/
-
 	// in this case the 'markers' are defined the top level
 	console.log(json.length);
+	// iterate through every 'marker'
+	$.each(json, function(index, currentJSON) {
+		/* MARKERS ATTRIBUTES
+			Count: 31
+			Country: "PT"
+			Latitude: 80
+			Longitude: 21
+			Name1: "YEAR"
+			Name2: "Gender"
+			Value1: "2004"
+			Value2: "T"
+			Var: "Active patients"
+			desc: "abc"
+		*/
+		// get the name of the country
+		var name = currentJSON['country'];
+		// get the count
+		var count = 0;
+		// get the latitude
+		var latitude = currentJSON['latitude'];
+		// get the longitude
+		var longitude = currentJSON['longitude'];
+		//get the description
+
+	});
 	var finalJSON;
 	return finalJSON;
 }
