bdb28be8921dfce15b3fb8a4286a320337bfcf21
Felgueiras <rafaelfelgueiras@ua.pt>
2015-07-07 16:01:11 +0100

    almost able to add regions to the map

--- js/regions.js
+++ js/regions.js
@@ -20,12 +20,49 @@ function addRegionsToMap(key) {
     console.log(isoCode);
     // find the country with that isoCode
     $.each(jsonCountries, function(index, currentCountry) {
-        if(currentCountry.Country == isoCode)
-        {
+        if (currentCountry.Country == isoCode) {
             // country was found, check if it has any region
-            if(currentCountry.Regions)
-            {
+            if (currentCountry.Regions) {
+                var regions = currentCountry.Regions;;
+                console.log(maps.maps[key]);
+                var regionColors = {};
+
+                // fill the array with colors
+                for(var regionIndex in regions){
+                    var currentRegionName = regions[regionIndex].name;
+                    regionColors[currentRegionName] = "#FF00FF";
+                }
+
+                console.log(regionColors);
                 // highlight the regions in the map
+                var myCustomColors = {
+                   'AU-SA': '#4E7387',
+                   'AU-WA': '#333333',
+                   'AU-VIC': '#89AFBF',
+                   'AU-TAS': '#817F8E',
+                   'AU-QLD': '#344B5E',
+                   'AU-NSW': '#344B5E',
+                   'AU-ACT': '#344B5E',
+                   'AU-NT': '#344B5E'
+               };
+               console.log(maps.maps[key].params.series);
+               maps.series.regions[0].setValues(myCustomColors);
+               //mapObject.series.regions[0].setVa  lues(regionColors);
+
+               /*
+               map = new jvm.WorldMap({
+                   map: 'au_merc_en',
+                   container: $('#ausie'),
+                   backgroundColor: '#eff7ff',
+                   series: {
+                       regions: [{
+                           attribute: 'fill'
+                       }]
+                   }
+               });
+
+
+                */
             }
         }
     });
