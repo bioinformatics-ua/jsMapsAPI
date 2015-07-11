6cfca47534c71b38f1ea6c3f24bb7c75c05cd1c3
Felgueiras <rafaelfelgueiras@ua.pt>
2015-07-11 11:49:33 +0100

    index.html now imports only one js file, source maps support is still missing

--- gulpfile.js
+++ gulpfile.js
@@ -8,18 +8,32 @@ var stripDebug = require('gulp-strip-debug');
 var uglify = require('gulp-uglify');
 
 // JS concat, strip debugging and minify
-gulp.task('scripts', function() {
+gulp.task('jsFiles', function() {
   gulp.src(['./js/main.js','./js/*.js'])
-    .pipe(concat('script.js'))
+    .pipe(concat('jsMapsApi.js'))
+    .pipe(stripDebug())
+    .pipe(uglify())
+    .pipe(gulp.dest('./finalJs'));
+});
+
+// generate maps
+gulp.task('maps', function() {
+  gulp.src(['./lib/jvectormap/maps/*.js'])
+    .pipe(concat('maps.js'))
     .pipe(stripDebug())
     .pipe(uglify())
     .pipe(gulp.dest('./finalJs'));
 });
 
 // default gulp task
-gulp.task('default', ['scripts'], function() {
-  // watch for JS changes
+gulp.task('default', ['jsFiles','maps'], function() {
+  // watch for changes on the API js files
   gulp.watch('./js/*.js', function() {
-    gulp.run('scripts');
+    gulp.run('jsFiles');
+  });
+
+  // watch for changes on the map files
+  gulp.watch('./lib/jvectormap/maps/*.js', function() {
+    gulp.run('maps');
   });
 });
