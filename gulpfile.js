42b32fa90fb4be73bde17c97c0fb0323adf9ea16
Felgueiras <rafaelfelgueiras@ua.pt>
2015-07-11 12:03:27 +0100

    added sourcemaps support to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -6,34 +6,36 @@ var changed = require('gulp-changed');
 var concat = require('gulp-concat');
 var stripDebug = require('gulp-strip-debug');
 var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
 
-// JS concat, strip debugging and minify
+// JS concat, minify and generate sourcemaps
 gulp.task('jsFiles', function() {
-  gulp.src(['./js/main.js','./js/*.js'])
-    .pipe(concat('jsMapsApi.js'))
-    //.pipe(stripDebug())
-    .pipe(uglify())
-    .pipe(gulp.dest('./finalJs'));
+    gulp.src(['./js/main.js', './js/*.js'])
+        .pipe(sourcemaps.init()) // Process the original sources
+        .pipe(concat('jsMapsApi.js'))
+        .pipe(uglify())
+        .pipe(sourcemaps.write()) // Add the map to modified source.
+        .pipe(gulp.dest('./finalJs'));
 });
 
 // generate maps
 gulp.task('maps', function() {
-  gulp.src(['./lib/jvectormap/maps/*.js'])
-    .pipe(concat('maps.js'))
-    .pipe(stripDebug())
-    .pipe(uglify())
-    .pipe(gulp.dest('./finalJs'));
+    gulp.src(['./lib/jvectormap/maps/*.js'])
+        .pipe(concat('maps.js'))
+        .pipe(stripDebug())
+        .pipe(uglify())
+        .pipe(gulp.dest('./finalJs'));
 });
 
 // default gulp task
-gulp.task('default', ['jsFiles','maps'], function() {
-  // watch for changes on the API js files
-  gulp.watch('./js/*.js', function() {
-    gulp.run('jsFiles');
-  });
+gulp.task('default', ['jsFiles', 'maps'], function() {
+    // watch for changes on the API js files
+    gulp.watch('./js/*.js', function() {
+        gulp.run('jsFiles');
+    });
 
-  // watch for changes on the map files
-  gulp.watch('./lib/jvectormap/maps/*.js', function() {
-    gulp.run('maps');
-  });
+    // watch for changes on the map files
+    gulp.watch('./lib/jvectormap/maps/*.js', function() {
+        gulp.run('maps');
+    });
 });
