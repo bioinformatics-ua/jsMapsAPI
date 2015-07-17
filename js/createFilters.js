b9b2e75b528c7a962cfc3a6d255a4ec91e1e640c
Felgueiras <rafaelfelgueiras@ua.pt>
2015-07-17 13:50:59 +0100

    fBoxes are now associated with a map - further improvements on the filtering are needed

--- js/createFilters.js
+++ js/createFilters.js
@@ -1,4 +1,7 @@
-var numFilters = 0;
+var FiltersBox = function(map, filterType) {
+    this.map = map;
+    this.filterType = filterType;
+}
 
 function resetFiltersBox() {
     // reset all the 'fboxes'
@@ -16,13 +19,83 @@ function resetFiltersBox() {
     addMarkersToMap();
 }
 
-function createFiltersBoxCheckboxes() {
+FiltersBox.prototype.createFiltersBoxWithEnumeration = function() {
+    $.each(this.filters, function(index, currentFilter) {
+        var filterName = currentFilter.name.toLowerCase();
+        filterName = filterName.charAt(0).toUpperCase() + filterName.slice(1);
+        var buttonId = 'dropdown' + index + 'button';
+        var ulId = 'dropdown' + index;
+        var toAppend = '';
+
+        // filter text
+        toAppend += '<p><b>' + filterName + ':</b></p>';
+        toAppend += '<div class="form-group">';
+        toAppend += '<input type="text" class="form-control" id="fbox' + index + '"';
+
+        // build the placeholder - check if we have continuous or discrete values
+        if (currentFilter.continuous)
+            toAppend += 'placeholder="' + currentFilter.min + '...' + currentFilter.max + '" +>';
+        else
+            toAppend += 'placeholder="' + currentFilter.values.join() + '" +>';
+        toAppend += '</div>';
+
+        $('filter-box').append(toAppend);
+
+        // add Bootstrap tooltip to the filters box
+        $('#filter-box').tooltip({
+            title: "Use this filter box to filter by multiple filters",
+            placement: "bottom"
+        });
+    });
+
+    // append errors
+    $('.form-control').append('<span class="glyphicon glyphicon-remove form-control-feedback">');
+
+    // add the buttons
+    var textToAppend = '<div id="filters_box">' +
+        '<button id="filter_box_apply_filters" type="button" class="btn btn-primary col-sm-4 col-sm-offset-1">Filter</button>' +
+        '<button id="filter_box_reset_filters" type="button" class="btn btn-primary col-sm-4 col-sm-offset-1">Reset</button></div>';
+    $('filter-box').append(textToAppend);
+
+    // triggered when the search button is clicked
+    $("#filter_box_apply_filters").click(function() {
+        // remove all the 'has-error' input boxes
+        restoreInputBoxes();
+        var jsonObject = {};
+        var numFilters = jsonFilters.length;
+        var emptyFilters = 0;
+        for (var i = 0; i < jsonFilters.length; i++) {
+            // current and next filter id's
+            var currentFilter = "#fbox" + i;
+            // current and next filter values
+            var currentFilterValue = $(currentFilter).val();
+            // check if we have any filtering to apply or not
+            if (currentFilterValue !== '') {
+                jsonObject[jsonFilters[i].name] = currentFilterValue;
+            } else {
+                emptyFilters++;
+            }
+        }
+        // avoid the user selecting the Filter button without inputing any data
+        if (emptyFilters != numFilters) {
+            filter(jsonObject);
+        }
+    });
+
+    // triggered when the reset button is clicked
+    $("#filter_box_reset_filters").click(function() {
+        restoreInputBoxes();
+        resetFiltersBox();
+    });
+}
+
+
+FiltersBox.prototype.createFiltersBoxCheckboxes = function() {
 
     $('filter-box').append('<ul class="nav navbar-nav" id="filterBoxCheckboxes" style="border-style: solid; border-width: 2px;"></ul>');
 
     // get all the filters
-    $.each(jsonFiltersArray, function(index, currentFilter) {
-        numFilters++;
+    $.each(this.filters, function(index, currentFilter) {
         var boxID = '#box' + (index + 1);
         // append to the HTML
         $('#filterBoxCheckboxes').append('<li class="col-sm-6" id="box' + (index + 1) + '" class="dropdown-checkbox-example dropdown-checkbox dropdown"></li>');
@@ -107,78 +180,6 @@ function createFiltersBoxCheckboxes() {
 
     // triggered when the reset button is clicked
     $("#filter_box_reset_filters").click(function() {
-        resetFiltersBox();
-    });
-}
-
-function createFiltersBoxWithEnumeration(jsonFilters) {
-    numFilters = jsonFilters.length;
-    // create filters box with enumeration
-    $.each(jsonFilters, function(index, currentFilter) {
-        var filterName = currentFilter.name.toLowerCase();
-        filterName = filterName.charAt(0).toUpperCase() + filterName.slice(1);
-        var buttonId = 'dropdown' + index + 'button';
-        var ulId = 'dropdown' + index;
-        var toAppend = '';
-
-        // filter text
-        toAppend += '<p><b>' + filterName + ':</b></p>';
-        toAppend += '<div class="form-group">';
-        toAppend += '<input type="text" class="form-control" id="fbox' + index + '"';
-
-        // build the placeholder - check if we have continuous or discrete values
-        if (currentFilter.continuous)
-            toAppend += 'placeholder="' + currentFilter.min + '...' + currentFilter.max + '" +>';
-        else
-            toAppend += 'placeholder="' + currentFilter.values.join() + '" +>';
-        toAppend += '</div>';
-
-        $('filter-box').append(toAppend);
-
-        // add Bootstrap tooltip to the filters box
-        $('#filter-box').tooltip({
-            title: "Use this filter box to filter by multiple filters",
-            placement: "bottom"
-        });
-    });
-
-    // append errors
-    $('.form-control').append('<span class="glyphicon glyphicon-remove form-control-feedback">');
-
-    // add the buttons
-    var textToAppend = '<div id="filters_box">' +
-        '<button id="filter_box_apply_filters" type="button" class="btn btn-primary col-sm-4 col-sm-offset-1">Filter</button>' +
-        '<button id="filter_box_reset_filters" type="button" class="btn btn-primary col-sm-4 col-sm-offset-1">Reset</button></div>';
-    $('filter-box').append(textToAppend);
-
-    // triggered when the search button is clicked
-    $("#filter_box_apply_filters").click(function() {
-        // remove all the 'has-error' input boxes
-        restoreInputBoxes();
-        var jsonObject = {};
-        var numFilters = jsonFilters.length;
-        var emptyFilters = 0;
-        for (var i = 0; i < jsonFilters.length; i++) {
-            // current and next filter id's
-            var currentFilter = "#fbox" + i;
-            // current and next filter values
-            var currentFilterValue = $(currentFilter).val();
-            // check if we have any filtering to apply or not
-            if (currentFilterValue !== '') {
-                jsonObject[jsonFilters[i].name] = currentFilterValue;
-            } else {
-                emptyFilters++;
-            }
-        }
-        // avoid the user selecting the Filter button without inputing any data
-        if (emptyFilters != numFilters) {
-            filter(jsonObject);
-        }
-    });
-
-    // triggered when the reset button is clicked
-    $("#filter_box_reset_filters").click(function() {
-        restoreInputBoxes();
         resetFiltersBox();
     });
 }
